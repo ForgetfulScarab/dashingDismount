4fc2b6f1e6c446cedc8b584bddeb5da7d69b2201
ForgetfulScarab <fogetfulscarab@hotmail.com>
2019-06-08 18:52:36 +0200

    Update dashingDismount.lua
    Removed the leftover debug print

--- dashingDismount.lua
+++ dashingDismount.lua
@@ -3,8 +3,6 @@ dashingDismount:RegisterEvent("UI_ERROR_MESSAGE")
 dashingDismount:SetScript("OnEvent", function(self, event, messageType)
 	local errorName = GetGameMessageInfo(messageType)
 	
-	print(errorName)
-	
 	if errorName == "ERR_SPELL_FAILED_S" or errorName == "ERR_ATTACK_MOUNTED" then
 		Dismount()
 	end
